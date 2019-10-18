c296f7bc0bfa3e10b177fd4527a3bd8bc7abcefe
Setareh Karimi <setarekarimi.kh@gmail.com>
2019-10-18 21:12:39 +0330

    Task: #2 Create User Model

---
+++ app/main/user/model/user.py
@@ -0,0 +1,37 @@
+import datetime
+
+from app.main import db, flask_bcrypt
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.INTEGER, primary_key=True)
+    phonenumber = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(100))
+    role = db.Column(db.Integer, default=1)
+    password = db.Column(db.String(1000))
+    createdAt = db.Column(db.DateTime)
+    updatedAt = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    lastLogin = db.Column(db.DateTime)
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.email)
+
+
+
+
+
+
