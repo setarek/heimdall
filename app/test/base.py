36a7a9bb1163d32d18b0713606fe983104d0a7a3
Setareh Karimi <setarekarimi.kh@gmail.com>
2019-10-23 17:01:54 +0330

    Change User Test Token

--- app/test/base.py
+++ app/test/base.py
@@ -1,11 +1,8 @@
-from datetime import datetime
 from flask_testing import TestCase
 
 from app.main import db
 from app.main import app
-from app.main.user.model.user import User
 
-test_user = 0
 class BaseTestCase(TestCase):
 
     def create_app(self):
@@ -16,19 +13,6 @@ class BaseTestCase(TestCase):
         db.create_all()
         db.session.commit()
 
-        user = User(
-            phonenumber='01234567890',
-            email='test@test.com',
-            firstname='bruce',
-            lastname='banner',
-            role=1,
-            createdAt=datetime.utcnow(),
-            updatedAt=datetime.utcnow(),
-            lastLogin=datetime.utcnow()
-        )
-        db.session.add(user)
-        db.session.commit()
-        test_user = user.id
     def tearDown(self):
         db.session.remove()
         db.drop_all()
--- app/test/test_user_token.py
+++ app/test/test_user_token.py
@@ -3,14 +3,31 @@ from datetime import datetime
 from app.main import db
 from app.main.user.model.user import User
 from app.utils.authentication import encode_auth_token, decode_auth_token
-from app.test.base import BaseTestCase, test_user
+from app.test.base import BaseTestCase
 
 class TestUserToken(BaseTestCase):
+    test_user = None # <--- 👈
 
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+        user = User(
+            phonenumber='01234567890',
+            email='test@test.com',
+            firstname='bruce',
+            lastname='banner',
+            role=1,
+            createdAt=datetime.utcnow(),
+            updatedAt=datetime.utcnow(),
+            lastLogin=datetime.utcnow()
+        )
+        db.session.add(user)
+        db.session.commit()
+        self.test_user = user.id
     def test_user_token(self):
 
-        auth_token = encode_auth_token(test_user)
+        auth_token = encode_auth_token(self.test_user)
 
         decoded_user = decode_auth_token(auth_token.decode())
-        self.assertTrue(decoded_user == test_user)
-
+        self.assertTrue(decoded_user == self.test_user)
