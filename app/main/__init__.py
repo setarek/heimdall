f7c27555b7c47d371e61e2a6a1ab503c65186992
Setareh Karimi <setarekarimi.kh@gmail.com>
2019-11-13 23:16:05 +0330

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -39,7 +39,7 @@ def create_app(config_name):
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = "serlina.karimi72@gmail.com"
-    app.config['MAIL_PASSWORD'] = "seka7296AI"
+    app.config['MAIL_PASSWORD'] = "000000"
     app.config['MAIL_DEFAULT_SENDER'] = "serlina.karimi72@gmail.com"
 
     # Logger config
